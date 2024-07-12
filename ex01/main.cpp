7bea39f52f00aa6ef3edec42baf3dbb21fc8eb2a
DriQii <enzovella6603@gmail.com>
2024-07-12 10:48:19 +0000

    finish

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,9 +3,11 @@
 int	main(void)
 {
 	Zombie *horde;
-	int		n = 42;
+	int		n = 2;
 
 	horde = zombieHorde(n, "enzo");
 	for(int i = 0; i < n; i++)
 		horde[i].announce();
+	delete [] horde;
+	return (0);
 }
